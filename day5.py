8f6390b025ddfadbfe834d0bb65bf5037b77e293
02rasjac <rasmusjacklin2002@gmail.com>
2022-12-05 16:26:43 +0100

    Day 5.2

--- day5.py
+++ day5.py
@@ -9,7 +9,6 @@ for n in range(1, n_stacks): # Initialize stack
 def readstack(line):
     stack_index = 0
     for i in range(1, stack_string_len, 4):
-        # print(stack_index)
         if line[i].isalpha():
             stacks[stack_index].append(line[i])
         stack_index += 1
@@ -24,19 +23,23 @@ with open("data/day5.txt") as f:
         else:
             break
 
-    # print(stacks)
     # Ingnore an empty line
     f.readline()
-    instruction_line = [] # [0, 1, 2] Move 0 from stack 1 to stack 2
 
     while True:
         line = f.readline()
         if not line: # End of file
             break
+        # [0, 1, 2] Move 0 from stack 1 to stack 2
         instruction_line = [int(c) for c in re.findall(r"\d+", line)]
-        for x in range(0, instruction_line[0]):
-            crate = stacks[instruction_line[1]-1].pop(0)
-            stacks[instruction_line[2]-1].insert(0, crate)
+        
+        # Crates to move
+        crates = stacks[instruction_line[1]-1][0:instruction_line[0]]
+        # Move crates on top of the requested stack
+        crates.extend(stacks[instruction_line[2]-1])
+        stacks[instruction_line[2]-1] = crates
+        # Remove moved crates from original stack
+        stacks[instruction_line[1]-1] = stacks[instruction_line[1]-1][(instruction_line[0]):]
 
 
 print(stacks)
