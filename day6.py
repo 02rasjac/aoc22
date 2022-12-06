dc973c9feb0bc36a33ef25fac2dc321bab38a859
02rasjac <rasmusjacklin2002@gmail.com>
2022-12-06 12:11:58 +0100

    Day 6.2

--- day6.py
+++ day6.py
@@ -2,14 +2,14 @@ stream = None
 with open("data/day6.txt") as f:
     stream = f.readline().strip("\n")
 
-buffer = list(stream[:4]) # Hold the 4 latest characters (oldest to newest)
+buffer = list(stream[:14]) # Hold the 14 latest characters (oldest to newest)
 
-message_start_index = 3
-for c in stream[4:]:
+message_start_index = 13
+for c in stream[14:]:
     message_start_index += 1
     buffer.pop(0) # Remove the oldest character
     buffer.append(c) # Add the new character to the end
-    if len(set(buffer)) == 4: # 4 unique characters
+    if len(set(buffer)) == 14: # 14 unique characters
         break
 
 print(message_start_index+1)
